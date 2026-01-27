113d2206e830079d567056e0eb6bb7778b531845
antongy-beep <anton.gybrant@gmail.com>
2026-01-27 15:40:27 +0100

    temporär generate funktion

--- lab1/rca.vhd
+++ lab1/rca.vhd
@@ -18,7 +18,19 @@ component fa
     port(
         a, b: in std_logic;
         cin: in std_logic;
-        cout: out std_logic
+        cout: out std_logic;
+        s: out std_logic
     );
 end component
 
+begin
+    G1: for i in 0 to 7 generate
+        adders: entity work.fa(dataflow) port map(
+            cin => cin
+            a => A(i)
+            b => B(i)
+            O(i) <= s
+            cout <= cout
+        );
+    end generate
+end structural
